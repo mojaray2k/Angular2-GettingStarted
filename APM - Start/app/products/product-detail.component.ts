959905626fa37ed8779fee22f4bf1ad2394005f4
Amen Moja Ra <amenra@Amens-Mac-mini.local>
2017-03-25 15:55:57 -0400

    adding route parameters

--- APM - Start/app/products/product-detail.component.ts
+++ APM - Start/app/products/product-detail.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { IProduct } from './products';
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html'
 })
-export class ProductDetailComponent {
-    pageTitle: string;
+export class ProductDetailComponent implements OnInit {
+    pageTitle: string = 'Product Detail';
     product: IProduct;
+
+    constructor(private _route: ActivatedRoute) {
+
+    }
+
+    ngOnInit() {
+        let id = this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
 }
