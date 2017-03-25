238d67880cc5f1fa7d35719d09156a9b24d0e212
Amen Moja Ra <amenra@Amens-Mac-mini.local>
2017-03-25 04:27:47 -0400

    Have learned how to retrieve data via a RESTful API

---
+++ APM - Start/app/products/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+
+import { IProduct } from './products';
+
+@Injectable()
+export class ProductService {
+    private _productUrl = 'api/products/products.json'; // This Could be https://imaginationeverywhere.info/wp-json/wp/v2/posts
+    constructor(private _http: Http) {}
+
+    getProducts(): Observable<IProduct[]> {
+        return this._http.get(this._productUrl)
+        .map((response: Response) => <IProduct[]> response.json())
+        .do( data => console.log('All: ' + JSON.stringify(data)))
+        .catch(this.handleError);
+    }
+
+    private handleError(error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+
+}
